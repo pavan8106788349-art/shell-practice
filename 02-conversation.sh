3e9d8369335200c6cdf5b5e541dba1d7963169f4
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-01-28 19:55:12 +0530

    conversatio.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh:: Hello Ramesh, How are you?''
+echo "Ramesh:: Hi Suresh, Iam fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning Ramesh?''
+echo "Ramesh:: Iam learning shell. What about you?''
