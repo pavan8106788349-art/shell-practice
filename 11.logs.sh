3f1c3c6c46b33e87e196ff216727b690c3b95628
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-01-29 19:58:33 +0530

    logs.sh

--- 11.logs.sh
+++ 11.logs.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/VAR/log/shell-script/$0.log"
+LOGS_FOLDER="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
@@ -11,13 +11,13 @@ fi
 
 mkdir -p $LOGS_FOLDER
 
-VALIDATE(){
-    if [ $1 -ne o ]; then
-        echo "$2 ... FAILURE"
-        exit 1
+vALIDATE(){
+    if [ $1 -ne 0 ]; then
+         echo "$2 ... FAILURE"
+         exit 1
     else
-       echo "$2 ... SUCCESS"
-    fi       
+        echo "$2 ... SUCCESS"
+    fi         
 }
 
 dnf install nginx -y &>> $LOGS_FILE
@@ -26,5 +26,5 @@ VALIDATE $? "Installing Nginx"
 dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing Mysql"
 
-dnf install nodejs &>> $LOGS_FILE
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Installing nodejs"
