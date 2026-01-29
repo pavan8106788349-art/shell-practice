d62eba98858178f019ac295149c672594f3de4a9
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-01-29 07:53:50 +0530

    conditions.sh

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
+NUMBER  =25
 
-NUMBER  =$1
-
-# -gt -> greater than
-# -lt -> less than
-# -eq -> equal
-# -ne -> not equal
 if [ $NUMBER -gt 20 ]; then
     echo "Given number: $NUMBER is greater than 20"
  fi
