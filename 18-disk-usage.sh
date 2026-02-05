731cd5602a967e9df4764720b5c7b2e03e12cfe0
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-02-05 17:01:07 +0530

    Deleted

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -14,8 +14,8 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-  USAGE=$($line | awk '{print $6}' | cut -d % -f1)
-  PARTITION=$($line | awk '{print $7}')
+  USAGE=$(echo $line | awk '{print $6}' | cut -d % -f1)
+  PARTITION=$(echo $line | awk '{print $7}')
 
   if [ "$USAGE" ge "$USAGE_THRESHOLD" ]; then
      MESSAGE+="High Disk usage on $PARTITION:$USAGE"
