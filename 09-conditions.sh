64035cba255fc4e3f78eac2089abd7facbefe3a9
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-18 21:12:25 +0530

    changes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,11 +2,12 @@
 
 NUMBER=$1
 
-if [ -z "$NUMBER" ]; then
-  echo "Please provide a number"
-  exit 1
-fi
-
-if [ "$NUMBER" -gt 20 ]; then
-  echo "Given number: $NUMBER is greater than 20"
-fi
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number: $NUMBER is equal to 20"
+fi    
