4ee1d5e5f701eef7d593a6af7bf01a89e007ffe9
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-18 21:26:51 +0530

    changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-USERID = $(id -u)
+USERID=$(id -u)
 
-if[ USERID -ne 0 ]; then
-   echo "Please run this script with root user access"
-fi   
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+fi
 
 echo "Installing Nginx"
-dnf install nginx -y   
+dnf install nginx -y
