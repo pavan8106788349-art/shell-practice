02e272d4ae9c91aa7bd705ecc2ac145483ae6dc1
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-18 22:40:05 +0530

    changes

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
@@ -16,4 +15,22 @@ if [ $? -ne 0 ]; then
     exit 1
 else
     echo "Installing Nginx ... SUCCESS"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs ... FAILURE"
+    exit 1
+else
+    echo "Installing nodejs ... SUCCESS"
 fi
