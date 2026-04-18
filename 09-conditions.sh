96e6a4c0ca9ce7acff14e8f9fd316796e1e649f2
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-18 21:08:30 +0530

    changes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,11 +1,11 @@
-!#/bin/bash
+#!/bin/bash
 
 NUMBER=$1
 
-gt -> greater than
-lt -> less than
-eq -> equal
-ne -> not equal
-if [$NUMBER -gt 20]; then
-  echo "Given Number: $NUMBER greater than 20"
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greater than 20"
 fi  
