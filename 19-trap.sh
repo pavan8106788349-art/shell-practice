b777e1caea695b724bbeb829fd08638718ef1248
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-04-19 12:15:48 +0530

    changes

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e
+set -e #ERR
+
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 echo "Hello World"
-echo "Iam learning shell"
-echoo "Printing error here"
+echo "I am learning Shell"
+echoo "printing error here"
 echo "No error in this"
